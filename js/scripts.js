b99b63b45961dccc3b06b04db9990ef8165d82d8
kanchankumari07 <98989464+kanchankumari07@users.noreply.github.com>
2022-06-10 09:33:14 -0600

    Update scripts.js
    added info about pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
- alert('Hello world');
-var name ="favorite food"
-var number = 1;
-document.write("pad thai is my favorite food");
+let pokemonlist=[
+{name:"Bulbasaur",
+  height:2,
+  type:['grass', 'poison'],
+
+
+name:'Venusaur',
+  height:2,
+  type: ['grass', 'poison'],
+
+
+   name: "weedle",
+  height:2,
+  type: ['bug','poison'],
+}
+
+]
+  console.log(pokemonlist);
