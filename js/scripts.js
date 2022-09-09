c07df60f51adcaeb9d96a5692e53433cdf93ea81
kanchankumari07 <98989464+kanchankumari07@users.noreply.github.com>
2022-09-09 12:34:52 -0600

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,74 +1,4 @@
 let pokemonRepository = (function () {
-    let repository = [
-        {
-            name: 'Bulbasaur',
-            height: 7,
-            type: ['grass', 'poison']
-        },
-        {
-            name: 'Squirtle',
-            height: 5,
-            type: ['water']
-        },
-        {
-            name: 'Charmander',
-            height: 4.5,
-            type: ['fire', 'flying']
-        }
-    ];
-
-    function add(pokemon) {
-        repository.push(pokemon);
-    }
-
-    function getAll() {
-        return repository;
-    }
-
-    function addListItem(pokemon) {
-        let $pokemonList = document.querySelector('.pokemon-list');
-        let $listItem = document.createElement('li')
-        let $button = document.createElement('button');
-        $button.innerHTML = pokemon.name
-
-        $pokemonList.appendChild($listItem);
-        $listItem.appendChild($button);
-        $button.classList.add('pokemon-button');
-        // event listener within function
-        // $button.addEventListener('click', function (e) {
-        //     showDetails(pokemon);
-        // })
-        clickPokemonButtonHandler($button, pokemon);
-
-    }
-    // Optional bonus
-    function clickPokemonButtonHandler(button, pokemonObject) {
-        button.addEventListener('click', function () {
-            showDetails(pokemonObject)
-        })
-    }
-
-    function showDetails(pokemon) {
-        console.log(pokemon)
-    }
-
-    return {
-        add: add,
-        getAll: getAll,
-        addListItem: addListItem
-    };
-})();
-
-
-
-
-
-pokemonRepository.getAll().forEach(function (pokemon) {
-    pokemonRepository.addListItem(pokemon)
-})  
-
-//task1.7//
-let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
@@ -151,6 +81,5 @@ pokemonRepository.loadList().then(function () {
     pokemonRepository.addListItem(pokemon);
   });
 });
-
 
 
