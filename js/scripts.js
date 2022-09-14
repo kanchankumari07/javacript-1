74c208d5b72dbb81417d6075d4ea80338d6ee1a6
kanchankumari07 <98989464+kanchankumari07@users.noreply.github.com>
2022-09-14 10:56:08 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -63,6 +63,64 @@ let pokemonRepository = (function () {
   function showDetails(item) {
     pokemonRepository.loadDetails(item).then(function () {
       console.log(item);
+         showModal(pokemon.name, + " height  " +  "img.Url")
+  });
+}
+  let modalContainer = document.querySelector('#modal-container');
+
+
+  function showModal(title, text, imageUrl) {
+    // Clear all existing modal content
+    modalContainer.innerHTML = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    // Add the new modal content
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+
+    let imageElement = document.createElement("img");
+    imageElement.setAttribute("src", img.url);
+    imageElement.setAttribute("width", "304");
+    imageElement.setAttribute("height", "228");
+    
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modal.appendChild(imageElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
+  }
+
+  function hideModal() {
+    modalContainer.classList.remove('is-visible');
+  }
+
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+  modalContainer.addEventListener('click', (e) => {
+    // Since this is also triggered when clicking INSIDE the modal container,
+    // We only want to close if the user clicks directly on the overlay
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
     });
   }
 
